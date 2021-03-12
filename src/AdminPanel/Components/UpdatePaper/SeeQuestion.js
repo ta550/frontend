c5acc4b8e9d6bdd4a3f902bcbea93bd49f3d661d
Hamza <itshamzajaved6@gmail.com>
2021-03-11 22:44:49 -0800

    The previous question appears for a while when moving from one to another.

--- src/AdminPanel/Components/UpdatePaper/SeeQuestion.js
+++ src/AdminPanel/Components/UpdatePaper/SeeQuestion.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Slide from '@material-ui/core/Slide';
-import { useSelector } from 'react-redux'
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Slide from "@material-ui/core/Slide";
+import { useSelector } from "react-redux";
 import { MathpixLoader, MathpixMarkdown } from "mathpix-markdown-it";
-import CheckCircleIcon from '@material-ui/icons/CheckCircle';
-import CancelIcon from '@material-ui/icons/Cancel';
-import S3 from 'react-aws-s3';
-import DeleteIcon from '@material-ui/icons/Delete';
-import ImagesCarouselModal from '../../../Modals/ImagesCarouselModal';
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import CancelIcon from "@material-ui/icons/Cancel";
+import S3 from "react-aws-s3";
+import DeleteIcon from "@material-ui/icons/Delete";
+import ImagesCarouselModal from "../../../Modals/ImagesCarouselModal";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
@@ -21,63 +21,54 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 export default function SeeQuestion(props) {
   const { handleClose, open } = props;
   const [data, setData] = React.useState([]);
-  const loginReducer = useSelector(state => state.loginReducer);
+  const loginReducer = useSelector((state) => state.loginReducer);
   const [question, setQuestion] = React.useState("");
   const [options, setOptions] = React.useState([]);
-  const [topics, setTopics] = React.useState([])
-  const [images, setImages] = React.useState([
-    {
-      imageurl: '',
-    }
-  ])
-  const [imageViewStatus, setImageViewStatus] = React.useState(false)
-
+  const [topics, setTopics] = React.useState([]);
+  const [images, setImages] = React.useState([]);
+  const [marks, setMarks] = React.useState("");
+  const [imageViewStatus, setImageViewStatus] = React.useState(false);
 
   const ImageViewClose = () => {
-    setImageViewStatus(false)
-  }
-
-
-
+    setImageViewStatus(false);
+  };
 
   React.useEffect(() => {
     if (window.SeeQuestionId !== undefined) {
-          setOptions([])
-          setTopics([])
-          setQuestion("")
-          setImages([])
+      setOptions([]);
+      setTopics([]);
+      setQuestion("");
+      setImages([]);
       fetch(`/dashboard/de/question/${window.SeeQuestionId}`, {
-        method: 'GET',
+        method: "GET",
         headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${loginReducer}`
-        }
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${loginReducer}`,
+        },
       })
-        .then(res => res.json())
-        .then(res => {
-          setQuestion(res.questions)
-          setOptions(res.options)
+        .then((res) => res.json())
+        .then((res) => {
+          setQuestion(res.questions);
+          setOptions(res.options);
+          setMarks(res.marks);
           if (res.images) {
-            setImages(res.images)
+            setImages(res.images);
           } else {
-            setImages([])
+            setImages([]);
           }
           if (res.topics) {
-            setTopics(res.topics)
+            setTopics(res.topics);
           } else {
-            setTopics([])
+            setTopics([]);
           }
-          console.log(res)
-          window.SeeQuestionId = undefined
+          console.log(res);
+          window.SeeQuestionId = undefined;
         })
-        .catch(err => console.log(err))
+        .catch((err) => console.log(err));
     } else {
       handleClose();
     }
-  }, [window.SeeQuestionId])
-
-
-
+  }, [window.SeeQuestionId]);
 
   return (
     <div>
@@ -88,101 +79,143 @@ export default function SeeQuestion(props) {
         TransitionComponent={Transition}
         keepMounted
         onClose={() => {
-          setOptions([])
-          setTopics([])
-          setQuestion("")
-          setImages([])
-          handleClose()
-        }
-        }
+          setOptions([]);
+          setTopics([]);
+          setQuestion("");
+          setImages([]);
+          handleClose();
+        }}
         aria-labelledby="alert-dialog-slide-title"
         aria-describedby="alert-dialog-slide-description"
       >
-        {
-          (question === "" ? (
-            <div className="d-flex align-items-center justify-content-center" style={{ height: '50vh' }}>
-              <div class="spinner-border" role="status">
+        {question === "" ? (
+          <div
+            className="d-flex align-items-center justify-content-center"
+            style={{ height: "50vh" }}
+          >
+            <div class="spinner-border" role="status">
               <span class="sr-only">Loading...</span>
             </div>
+          </div>
+        ) : (
+          <DialogContent>
+            <h5 className="py-3 simple-header-font font-italic font-weight-bold shadow text-center">
+              Marks = {marks}
+            </h5>
+            <div style={{ fontSize: "15px" }}>
+              <MathpixLoader>
+                <MathpixMarkdown text={question} />
+              </MathpixLoader>
             </div>
-          ): (
-      <DialogContent>
-        <div style={{ fontSize: "15px" }}>
-          <MathpixLoader>
-            <MathpixMarkdown text={question} />
-          </MathpixLoader>
-        </div>
-        <hr />
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-md-6">
-              <h5 className="py-3 simple-header-font font-italic font-weight-bold shadow text-center">Options</h5>
-              <div>
-                {options.map((item, i) => (
-                  <div className="pt-3 d-flex" style={{ borderBottom: "1px solid rgba(0,0,0,0.3)" }}>
-                    {(item.correct === true) ? <CheckCircleIcon className="text-success" /> : <CancelIcon className="text-danger" />}
-                    <p> &nbsp;{item.option}</p>
+            <hr />
+            <div className="container-fluid">
+              <div className="row">
+                <div className="col-md-6">
+                  <h5 className="py-3 simple-header-font font-italic font-weight-bold shadow text-center">
+                    Options
+                  </h5>
+                  <div>
+                    {options.map((item, i) => (
+                      <div
+                        className="pt-3 d-flex"
+                        style={{ borderBottom: "1px solid rgba(0,0,0,0.3)" }}
+                      >
+                        {item.correct === true ? (
+                          <CheckCircleIcon className="text-success" />
+                        ) : (
+                          <CancelIcon className="text-danger" />
+                        )}
+                        <p> &nbsp;{item.option}</p>
+                      </div>
+                    ))}
                   </div>
-                ))}
-              </div>
-            </div>
-            <div className="col-md-6">
-              <h5 className="py-3 simple-header-font font-italic font-weight-bold shadow text-center">Topics</h5>
-              <div>
-                {
-                  (topics.length === 0 ? (
-                    <h5 className="text-center">This question does not have any topic</h5>
-                  ) : (
+                </div>
+                <div className="col-md-6">
+                  <h5 className="py-3 simple-header-font font-italic font-weight-bold shadow text-center">
+                    Topics
+                  </h5>
+                  <div>
+                    {topics.length === 0 ? (
+                      <h5 className="text-center">
+                        This question does not have any topic
+                      </h5>
+                    ) : (
                       topics.map((item, i) => (
-                        <div className="pt-3 d-flex" style={{ borderBottom: "1px solid rgba(0,0,0,0.3)" }}>
+                        <div
+                          className="pt-3 d-flex"
+                          style={{ borderBottom: "1px solid rgba(0,0,0,0.3)" }}
+                        >
                           <p> &nbsp;{item.topic}</p>
                         </div>
                       ))
-                    )
-                  )
-                }
-              </div>
-            </div>
-          </div>
-          <br />
-          <div className="row">
-            <h5 className="col-12 simple-header-font font-italic font-weight-bold shadow text-center py-3">Related Images ({images.length})</h5>
-            {
-              (images.length === 0 ? (
-                <div className="col-12">
-                  <h5 className="text-center">This question does not have any image</h5>
+                    )}
+                  </div>
                 </div>
-              ) : (
+              </div>
+              <br />
+              <div className="row">
+                <h5 className="col-12 simple-header-font font-italic font-weight-bold shadow text-center py-3">
+                  Related Images ({images.length})
+                </h5>
+                {images.length === 0 ? (
+                  <div className="col-12">
+                    <h5 className="text-center">
+                      This question does not have any image
+                    </h5>
+                  </div>
+                ) : (
                   images.map((item, i) => {
                     if (item.imageurl) {
-                      return <div className="position-relative p-2 d-flex align-items-center col-2" style={{ cursor: 'pointer' }} onClick={() => { window.activeImageinImageCarousel = i; setImageViewStatus(true) }}>
-                        <img alt="Image Error" style={{ height: '120px', width: '100%' }} className="img-fluid px-1" src={item.imageurl} />
-                      </div>
+                      return (
+                        <div
+                          className="position-relative p-2 d-flex align-items-center col-2"
+                          style={{ cursor: "pointer" }}
+                          onClick={() => {
+                            window.activeImageinImageCarousel = i;
+                            setImageViewStatus(true);
+                          }}
+                        >
+                          <img
+                            alt="Image Error"
+                            style={{ height: "120px", width: "100%" }}
+                            className="img-fluid px-1"
+                            src={item.imageurl}
+                          />
+                        </div>
+                      );
                     }
                   })
-                ))
-            }
-            {/* Images View Carousel Dialog */}
-            {
-              (images.lenght === 0) ? (
-                <div></div>
-              ) : (
-                  <ImagesCarouselModal open={imageViewStatus} handleClose={ImageViewClose} data={images} />
-                )
-            }
-          </div>
-        </div>
-      </DialogContent>
-  ))
-}
+                )}
+                {/* Images View Carousel Dialog */}
+                {images.lenght === 0 ? (
+                  <div></div>
+                ) : (
+                  <ImagesCarouselModal
+                    open={imageViewStatus}
+                    handleClose={ImageViewClose}
+                    data={images}
+                  />
+                )}
+              </div>
+            </div>
+          </DialogContent>
+        )}
 
-
-      <DialogActions>
-        <Button onClick={handleClose} color="primary">
-          Cancel
+        <DialogActions>
+          <Button
+            onClick={() => {
+              setOptions([]);
+              setTopics([]);
+              setQuestion("");
+              setImages([]);
+              handleClose();
+            }}
+            color="primary"
+          >
+            Cancel
           </Button>
-      </DialogActions>
-      </Dialog >
-    </div >
+        </DialogActions>
+      </Dialog>
+    </div>
   );
 }
--- src/Modals/ImagesCarouselModal.js
+++ src/Modals/ImagesCarouselModal.js
@@ -1,142 +1,123 @@
-import React from 'react';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import MobileStepper from '@material-ui/core/MobileStepper';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Slide from '@material-ui/core/Slide';
+import React from "react";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import MobileStepper from "@material-ui/core/MobileStepper";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+import Dialog from "@material-ui/core/Dialog";
+import Slide from "@material-ui/core/Slide";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
-    return <Slide direction="up" ref={ref} {...props} />;
+  return <Slide direction="up" ref={ref} {...props} />;
 });
 
-
-const data2 = [
-    {
-        label: 'San Francisco – Oakland Bay Bridge, United States',
-        imgPath:
-            'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
-    },
-    {
-        label: 'Bird',
-        imgPath:
-            'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
-    },
-    {
-        label: 'Bali, Indonesia',
-        imgPath:
-            'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80',
-    },
-    {
-        label: 'NeONBRAND Digital Marketing, Las Vegas, United States',
-        imgPath:
-            'https://images.unsplash.com/photo-1518732714860-b62714ce0c59?auto=format&fit=crop&w=400&h=250&q=60',
-    },
-    {
-        label: 'Goč, Serbia',
-        imgPath:
-            'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
-    },
-];
-
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    header: {
-        display: 'flex',
-        alignItems: 'center',
-        height: 50,
-        paddingLeft: theme.spacing(4),
-        backgroundColor: theme.palette.background.default,
-    },
-    img: {
-        height: 500,
-        overflow: 'hidden',
-        display: 'block',
-        width: '100%',
-    },
+  root: {
+    flexGrow: 1,
+  },
+  header: {
+    display: "flex",
+    alignItems: "center",
+    height: 50,
+    paddingLeft: theme.spacing(4),
+    backgroundColor: theme.palette.background.default,
+  },
+  img: {
+    overflow: "hidden",
+    display: "block",
+    width: "100%",
+    maxHeight: "70vh",
+  },
 }));
 
 const ImagesCarouselModal = (props) => {
-    const { data, open, handleClose } = props;
-    const classes = useStyles();
-    const theme = useTheme();
-    const [activeStep, setActiveStep] = React.useState(0);
-    const maxSteps = data.length;
+  const { data, open, handleClose } = props;
+  const classes = useStyles();
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = data.length;
 
-    React.useEffect(() => {
-        if (window.activeImageinImageCarousel !== undefined) {
-            setActiveStep(window.activeImageinImageCarousel)
-        }
-    },[window.activeImageinImageCarousel])
-
-    const handleNext = () => {
-        setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    };
-
-    const handleBack = () => {
-        setActiveStep((prevActiveStep) => prevActiveStep - 1);
-    };
-    if (!data[activeStep]) {
-        return (
-            <div></div>
-        )
-    } else {
-
-        return (
-            <div>
-                <Dialog
-                    open={open}
-                    fullWidth={true}
-                    maxWidth="sm"
-                    TransitionComponent={Transition}
-                    keepMounted
-                    onClose={handleClose}
-                    aria-labelledby="alert-dialog-slide-title"
-                    aria-describedby="alert-dialog-slide-description"
-                >
-                    {/* Image View Start */}
-                    <div className={classes.root}>
-                        <Paper square elevation={0} className={classes.header}>
-                            <Typography>{data[activeStep].imageurl.split('/').pop()}</Typography>
-                        </Paper>
-                        <img
-                            className={classes.img}
-                            src={data[activeStep].imageurl}
-                            alt={data[activeStep].label}
-                        />
-                        <MobileStepper
-                            steps={maxSteps}
-                            position="static"
-                            variant="text"
-                            activeStep={activeStep}
-                            nextButton={
-                                <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
-                                    Next
-                                    {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-                                </Button>
-                            }
-                            backButton={
-                        <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-                            {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-                            Back
-                        </Button>
-                            }
-                        />
-                    </div>
-                </Dialog>
-            </div>
-        );
+  React.useEffect(() => {
+    if (window.activeImageinImageCarousel !== undefined) {
+      setActiveStep(window.activeImageinImageCarousel);
     }
-}
+  }, [window.activeImageinImageCarousel]);
 
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+  if (!data[activeStep]) {
+    return <div></div>;
+  } else {
+    return (
+      <div>
+        <Dialog
+          open={open}
+          fullWidth={true}
+          maxWidth="sm"
+          TransitionComponent={Transition}
+          keepMounted
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-slide-title"
+          aria-describedby="alert-dialog-slide-description"
+        >
+          {/* Image View Start */}
+          <div className={classes.root}>
+            <Paper square elevation={0} className={classes.header}>
+              <Typography>
+                {data[activeStep].imageurl.split("/").pop()}
+              </Typography>
+            </Paper>
+            <img
+              className={`${classes.img} py-5`}
+              src={data[activeStep].imageurl}
+              alt={data[activeStep].label}
+            />
+            <MobileStepper
+              steps={maxSteps}
+              position="static"
+              variant="text"
+              activeStep={activeStep}
+              nextButton={
+                <Button
+                  size="small"
+                  onClick={handleNext}
+                  disabled={activeStep === maxSteps - 1}
+                >
+                  Next
+                  {theme.direction === "rtl" ? (
+                    <KeyboardArrowLeft />
+                  ) : (
+                    <KeyboardArrowRight />
+                  )}
+                </Button>
+              }
+              backButton={
+                <Button
+                  size="small"
+                  onClick={handleBack}
+                  disabled={activeStep === 0}
+                >
+                  {theme.direction === "rtl" ? (
+                    <KeyboardArrowRight />
+                  ) : (
+                    <KeyboardArrowLeft />
+                  )}
+                  Back
+                </Button>
+              }
+            />
+          </div>
+        </Dialog>
+      </div>
+    );
+  }
+};
 
 export default ImagesCarouselModal;
